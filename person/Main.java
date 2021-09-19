ab04b9d8c0970a090d132392a97f2bdda11ea70e
AlibekNematov <85082765+AlibekNematov@users.noreply.github.com>
2021-09-20 02:19:14 +0300

    Update Main.java

--- person/Main.java
+++ person/Main.java
@@ -2,7 +2,7 @@ package com.person;
 
 public class Main extends Person {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("Press enter to start");
         ClientTest.clientTest();
         PersonTest.testPerson();
